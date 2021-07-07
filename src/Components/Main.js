c0bb4df330a87c9af3e2a89f0a538f8945da30fe
ishukhanchi <ishukhanchi@gmail.com>
2021-07-07 16:25:34 +0530

    update state change

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,16 +7,36 @@ const DEFAULT_ARRAY = [
 ];
 
 const Main = () => {
+  //Defining Hook to store current state as 2-D array and setArr to update the current state
   const [arr, setArr] = useState(DEFAULT_ARRAY);
+
+  //to change the state
+  const handleChangeState = (x, y) => {
+    // const newState = arr.map((x) => {
+    //   return x.slice();
+    // });
+    const newState = arr.map((x) => x.slice());
+    newState[x][y] = 1;
+    setArr(newState);
+    console.log(x, y);
+  };
 
   return (
     //to return 3x3 matrix rendering 9 buttons
     <div>
-      {arr.map((value, index) => {
+      {arr.map((value, x) => {
         return (
           <div>
-            {value.map((item, count) => {
-              return <button>Hi</button>;
+            {value.map((item, y) => {
+              return (
+                <button
+                  onClick={() => {
+                    handleChangeState(x, y);
+                  }}
+                >
+                  {arr[x][y]}
+                </button>
+              );
             })}
           </div>
         );
